897f7e3fd358d2d5e60c05ad039d73e29568ad41
Riknesh Poonithan <laxmerik007@gmail.com>
2026-02-22 16:31:09 +0800

    Add leap year checker in Q1.R

---
+++ L5/Q1.R
@@ -0,0 +1,7 @@
+year <- as.integer(readline(prompt = "Input year: "))
+
+if ((year %% 4 == 0 && year %% 100 != 0) || (year %% 400 == 0)) {
+  cat(year, "is a leap year.")
+} else {
+  cat(year, "is a not leap year.")
+}
